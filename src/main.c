51901b7ad963233d924110cfbd57f98693c7de6e
unknown <mosolova.viktoriya@mail.ru>
2023-02-13 14:43:49 +0300

    add mjollror branch

--- src/main.c
+++ src/main.c
@@ -11,24 +11,6 @@ int main() {
     setSign(&a, sign1); setSign(&b, sign2);
     setScale(&a, scale1); setScale(&b, scale2);
     printDecAndBin(a); printDecAndBin(b);
-    // printf("\nless\n");
-    // if (s21_is_less(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
-    // printf("\nless_or_equal\n");
-    // if (s21_is_less_or_equal(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
-    // printf("\ngreater\n");
-    // if (s21_is_greater(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
-    // printf("\ngreater_or_equal\n");
-    // if (s21_is_greater_or_equal(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
-    // printf("\nequal\n");
-    // if (s21_is_equal(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
-    // printf("\nnot_equal\n");
-    // if (s21_is_not_equal(a,b)) printf("TRUE\n");
-    // else printf("FALSE\n");
 
     return 0;
 }
