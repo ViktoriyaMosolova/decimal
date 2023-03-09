30da4eb14b7aae7bb05ed38dd0bee2b76cbe7b03
floridac <floridac@student.21-school.ru>
2023-03-09 21:43:28 +0300

    fix_round

--- src/main.c
+++ src/main.c
@@ -19,14 +19,14 @@ void run_all_testcases() {
                          //suite_s21_is_less(),
                          //suite_s21_is_equal(),
                          //suite_s21_is_not_equal(),
-                         //suite_s21_from_decimal_to_float(),
+                         suite_s21_from_decimal_to_float(),
                          //suite_s21_from_float_to_decimal(),
                          //suite_s21_from_decimal_to_int(),
                          //suite_s21_from_int_to_decimal(),
                          //suite_s21_truncate(),
                          //suite_s21_floor(),
                          //suite_s21_negate(),
-                         suite_s21_round(),
+                         //suite_s21_round(),
                          NULL};
 
   for (int i = 0; list_cases[i] != NULL; i++) {
